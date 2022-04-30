76660bcad5fe0414e7d6047b6ed4742ff582b88d
SNORLAX <vincent0331@outlook.com>
2022-04-30 13:34:25 +0800

    2

---
+++ src/main/java/com/snorlax/jenkinsdemo/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.snorlax.jenkinsdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Snorlax
+ * @Date: 2022/4/30 13:31
+ * @Version: 1.0
+ * @History:
+ * @Note:
+ */
+@RestController
+public class HelloController {
+    @GetMapping("hello")
+    public String sayHello(){
+        return "hello customer";
+    }
+}
